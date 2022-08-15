7f52d681858e45a36fcdf5f066619f8132d28770
FatimahAlghamdi <57475085+fatimahmalghamdi@users.noreply.github.com>
2022-08-15 22:52:55 +0300

    Super Ninja Assignment

---
+++ ninja.js
@@ -0,0 +1,43 @@
+class Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 0;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(this.name);
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+
+    showStats(){
+        console.log("Name: " + this.name + " Strength: " + this.strength + " Speed: " + this.speed + " Health: " + this.health); 
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months");
+    }
+}
+
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
